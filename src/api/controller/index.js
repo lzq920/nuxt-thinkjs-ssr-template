efa801ff1f3bd21f502d5b8c05f5394f5f030b86
eleven <lzq920@vip.qq.com>
2018-08-22 14:46:41 +0800

    add axios

--- src/api/controller/index.js
+++ src/api/controller/index.js
@@ -1,9 +1,28 @@
 const Base = require("./base.js");
-
+const qs = require("querystring");
+const axios = require("axios");
 module.exports = class extends Base {
-  indexAction() {
-    return this.success({
-      title: "index"
-    });
+  async indexAction() {
+    await axios({
+      url: "https://cloud.dev.100cbc.com/admin/user/login",
+      method: "post",
+      data: qs.stringify({
+        Phone: "18888888888",
+        Password: "123456"
+      }),
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    })
+      .then(res => {
+        this.success(res.status, res.data);
+      })
+      .catch(err => {
+        if (think.isArray(err.response.data)) {
+          this.fail(err.response.status, err.response.data[0].message);
+        } else {
+          this.fail(err.response.status, err.response.data.message);
+        }
+      });
   }
 };
